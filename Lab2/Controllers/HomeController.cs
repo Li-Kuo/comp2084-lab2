43192dbee04f291aea9c64896276a0a150d6ab84
Li-Kuo <200414666@student.georgianc.on.ca>
2019-09-17 15:15:35 -0400

    add About

--- Lab2/Controllers/HomeController.cs
+++ Lab2/Controllers/HomeController.cs
@@ -20,6 +20,11 @@ namespace Lab2.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
